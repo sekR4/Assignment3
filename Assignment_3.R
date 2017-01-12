322d82af7be1b24c9850bfe91e45c8a40f6bb0fe
Sebastian Kraus <sebastian.kraus.jena@gmail.com>
2017-01-12 17:36:55 +0900

    trying to get it work

--- Assignment_3.R
+++ Assignment_3.R
@@ -41,12 +41,45 @@ best <- function(state, outcome) {
                 stop(print("invalid outcome"))
         }
         
-        ## Return hospital name in that state with lowest 30-day death
-        ## rate
+        ## Return hospital name in that state with lowest 30-day death rate
+        #(dummy-state, for creating the function)
+        state <- "AL"
+        # Subsetting by state
+        sub.data <- subset(data.all, State == state)
         
-        sub.data <- split(data.all, data.all$State)
-        sub.data.o <- 
+        # Getting the right outcome
+        if (outcome == "heart attack") {
+                out.c <- 11
+        }
         
+        else if (outcome == "heart failure") {
+                out.c <- 17
+        }
         
+        else {
+                out.c <- 23
+        }
         
+        # Getting required data without NA's
+        data.req <- sub.data[,out.c]
+#       data.req <- data.req[complete.cases(data.req),]
+        
+        # Print the hospital name where the death rate has it's miniumum
+        minimum <- min(data.req)
+        
+        data.all[which(data.all[data.req,2] == minimum)]
 }
+
+
+
+data.all <- read.csv("rprog_data/outcome-of-care-measures.csv", 
+                     na.strings = "Not Available", stringsAsFactors = FALSE)
+
+sub.data <- split(data.all, data.all$State)
+head(sub.data)
+str(sub.data)
+summary(sub.data)
+
+min(complete.cases(sub.data$AK[,11]))
+
+data.all$State[which(data.all$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == min(complete.cases(sub.data$AK[,11])))]
